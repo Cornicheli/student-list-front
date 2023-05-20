e9f7bebacdebdd474819a966cd3ea13e61110730
cornicheli <gabrielcornide.pg@gmail.com>
2023-05-20 01:27:49 -0300

    create new student and search

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,84 +8,73 @@ const List = () => {
     const [busqueda, setBusqueda] = useState('')
     const [students, setStudents] = useState([])
     const [order , setOrder ] = useState('asc')
-    const [newStudentData, setNewStudentData] = useState({
+    const [filteredStudents, setFilteredStudents] = useState([]);
+    const [newStudent, setNewStudent] = useState({
         nombre : '',
         apellido: '',
         edad: 0
     })
 
+    // Obtener la lista de estudiantes desde la API al cargar el componente
     const getStudents = async () => {
-        const alumno = await axios.get(`${BASE_URL}/alumno/read`)
-        console.log(alumno.data.res)
-        const usersMap = alumno.data.res
-        setStudents(usersMap)
-        console.log(usersMap)
+        try {
+            const alumno = await axios.get(`${BASE_URL}/alumno/read`)
+            console.log(alumno.data.res)
+            const usersMap = alumno.data.res
+            setStudents(usersMap)
+            console.log(usersMap)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    const createStudent  = async () => {
+    // Crear un nuevo estudiante
+    const createStudent  = async (e) => {
+        e.preventDefault()
         try {
-            
-            const alumno = await axios.post(`${BASE_URL}/alumno/create`, newStudentData)
+            const alumno = await axios.post(`${BASE_URL}/alumno/create`, newStudent)
             console.log('Alumno creado:', alumno.data);
             const createdStudent = alumno.data
-            setStudents([...students], createdStudent)
-
+            setStudents([...students, createdStudent])
         } catch (error) {
             console.error('Error al crear alumno:', error);
         }
+    }
+
+    // Actualizar el estado cuando cambian los campos del formulario de creación
+    const handleInputChange = (e) => {
+        e.preventDefault();
+        const { name, value } = e.target
+        let newStudentData = { ...newStudent, [name]: value }
+        setNewStudent(newStudentData)
+        console.log(newStudentData)
+    }
+
+    // Filtrar la lista de estudiantes por nombre o apellido
+    const filter = (termSearch) => {
+    const resultSearch = students.filter((elemento) => {
+        if (
+            elemento.nombre.toString().toLowerCase().includes(termSearch.toLowerCase()) ||
+            elemento.apellido.toString().toLowerCase().includes(termSearch.toLowerCase())
+        ) {
+            return elemento
+        }
+    })
         
+        if (termSearch === '') {
+            setFilteredStudents([]); // Restablecer la lista de estudiantes filtrados
+        } else {
+            setFilteredStudents(resultSearch)
+        }
     }
 
-    const deleteStudent = async (id) => {
-        await axios.delete(`${BASE_URL}/alumno/delete/${id}`);
-        const filteredStudents = students.filter(student => student.id !== id);
-        setStudents(filteredStudents);
-    }
-
-    const updateStudent = async (id, updateStudent) => {
-        await axios.put(`${BASE_URL}/alumno/update/${id}`, updateStudent)
-        const updatedStudents = students.map(student => {
-            if(student.id === id){
-                return { ...student, ...updatedStudents} 
-            }
-        })
-    }
-
+    // Manejar el cambio en el campo de búsqueda
     const handleChange = e => {
         setBusqueda(e.target.value)
         filter(e.target.value)
     }
 
-    const handleInputChange = (e) => {
-        setNewStudentData({
-            ...newStudentData, [e.target.name] : e.target.value
-        })
-    }
-
-    const handleCreateClick = () => {
-        createStudent();
-      };
-
-    const resetList = () => {
-        getStudents();
-    }
-
-    const filter = (termSearch) => {
-        const resultsSearch = students.filter((elemento) => {
-            if (
-                elemento.nombre.toString().toLowerCase().includes(termSearch.toLowerCase())
-                || elemento.apellido.toString().toLowerCase().includes(termSearch.toLowerCase())
-            ) {
-                return elemento
-            }
-        });
-        setStudents(resultsSearch)
-
-        if (termSearch === '') {
-            resetList();
-        }
-    }
-
+    // Ordenar la lista de estudiantes por edad en orden ascendente o descendente
     const orderForAge = () => {
         const ordenStudents = [...students].sort((a, b) => {
             if(order === 'asc') {
@@ -98,9 +87,11 @@ const List = () => {
         setOrder(order === 'asc' ? 'des' : 'asc')
     }
 
+    // Obtener la lista de estudiantes al cargar el componente
     useEffect(() => {
         try {
             getStudents()
+            filter(busqueda); // Agrega esta línea para actualizar la lista de estudiantes filtrados al cargar la página
         } catch (error) {
             console.log(error)
         }
@@ -111,41 +102,64 @@ const List = () => {
             <NavBar />
 
             <div className='d-flex p-2'>
-                <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                {/* Modal para agregar un nuevo estudiante */}
+                <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div className="modal-dialog">
                         <div className="modal-content">
                             <div className="modal-header">
-                                <h5 className="modal-title" id="staticBackdropLabel">Modal title</h5>
+                                <h5 className="modal-title" id="staticBackdropLabel">Nuevo Alumno</h5>
                                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className="modal-body">
-                                <form className="row g-3">
+                                {/* Formulario para agregar un nuevo estudiante */}
+                                <form className="row g-3" onSubmit={createStudent}>
+
                                     <div className="col-auto">
-                                        <input type="text"  className="form-control-plaintext" id="staticEmail2" placeholder="nombre" />
+                                        {/* Campo de entrada para el nombre */}
+                                        <input type="text"
+                                            className="form-control-plaintext"
+                                            name='nombre' onChange={handleInputChange}
+                                            id="staticEmail2" placeholder="nombre"
+                                        />
+
                                     </div>
                                     <div className="col-auto">
-                                        <input type="text"  className="form-control-plaintext" id="staticEmail2" placeholder="apellido" />
+                                        {/* Campo de entrada para el apellido */}
+                                        <input type="text"
+                                            className="form-control-plaintext"
+                                            name="apellido" onChange={handleInputChange}
+                                            id="staticEmail2" placeholder="apellido"
+                                        />
                                     </div>
+
                                     <div className="col-auto">
-                                        <input type="number" className="form-control" id="inputPassword2" placeholder="edad" />
+                                        {/* Campo de entrada para la edad */}
+                                        <input type="number"
+                                            className="form-control"
+                                            name='edad' onChange={handleInputChange}
+                                            id="inputPassword2" placeholder="edad"
+                                        />
+
                                     </div>
+
                                     <div className="col-auto">
-                                        <button type="submit" className="btn btn-primary mb-3">Confirm Edit</button>
+                                        {/* Botón para agregar un nuevo estudiante */}
+                                        <button type="submit" className="btn btn-primary mb-3"> Agregar </button>
                                     </div>
+
                                 </form>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Understood</button>
                             </div>
                         </div>
                     </div>
                 </div>
+                {/* Campo de búsqueda */}
                 <input className="form-control me-2" type="search" onChange={handleChange} value={busqueda} placeholder="Search" aria-label="Search" />
+                {/* Botón para abrir el modal de creación */}
                 <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                     Edit
                 </button>
             </div>
+            {/* Tabla de estudiantes */}
             <table className="table table-bordered border-primary">
                 <thead>
                     <tr>
@@ -155,15 +169,26 @@ const List = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {
-                        students.map(item => (
+                {
+                    
+                    filteredStudents.length > 0
+                        ? filteredStudents.map((item) => (
+                            // Restablecer la lista para cada estudiante
                             <tr key={item.id}>
-                                <td> {item.nombre} </td>
-                                <td> {item.apellido} </td>
-                                <td> {item.edad} </td>
+                            <td>{item.nombre}</td>
+                            <td>{item.apellido}</td>
+                            <td>{item.edad}</td>
                             </tr>
                         ))
-                    }
+                        : students.map((item) => (
+                            // Lista de la tabla para cada estudiante
+                            <tr key={item.id}>
+                            <td>{item.nombre}</td>
+                            <td>{item.apellido}</td>
+                            <td>{item.edad}</td>
+                            </tr>
+                        ))
+                }
                 </tbody>
             </table>
         </>
@@ -171,3 +196,21 @@ const List = () => {
 }
 
 export default List
+
+
+
+
+    // const deleteStudent = async (id) => {
+    //     await axios.delete(`${BASE_URL}/alumno/delete/${id}`);
+    //     const filteredStudents = students.filter(student => student.id !== id);
+    //     setStudents(filteredStudents);
+    // }
+
+    // const updateStudent = async (id, updateStudent) => {
+    //     await axios.put(`${BASE_URL}/alumno/update/${id}`, updateStudent)
+    //     const updatedStudents = students.map(student => {
+    //         if(student.id === id){
+    //             return { ...student, ...updatedStudents} 
+    //         }
+    //     })
+    // }
